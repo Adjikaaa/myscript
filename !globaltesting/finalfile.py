5e7e82d624b1b9c8f3c2ecd5de18f0ae05dd35d2
Adjikaaa <adjika88@gmail.com>
2025-03-17 18:44:41 +0300

    hj

--- !globaltesting/finalfile.py
+++ !globaltesting/finalfile.py
@@ -4,63 +4,93 @@ from collections import defaultdict
 def func(N):
 	dirname = pathlib.Path(N)
 	os.chdir(dirname)
+	#считываются данные из файла в формате: число решений, кнф  далее строки с решениями
 	f = open(N + '.txt', "r")
-	result = open(N + 'res.txt', 'a')
+
+	#файл res для записи результата работы функции
+	result = open(N + 'res.txt', 'w')
+
+	#считываем все строки файла в список
 	forparsing = []
 	for line in f:
 		forparsing.append(line[:-1])
 #	print(*forparsing, sep = '\n')
 
-	dd = defaultdict(list)
+	#заполняем словарь, {количество решений:{название кнф файла:конфигурация решений}}
+	dd = defaultdict(dict)
 	for i in range(len(forparsing)):
 		if 'for' in forparsing[i]:
+			#парсим строку и выделяем кол-во решений и название кнф
 			l = int(forparsing[i][0:2])
 			cnf = '(' + forparsing[i][(forparsing[i].index('r') + 2):(forparsing[i].index('n') + 2)] + ')'
+			#сохраняем конфигурацию решений этой кнф в отдельный временный перезаписываемый массив
 			val = []
 			for j in range(l):
 				line = forparsing[i + 1 + j][1:-1]
 				val.append(line)
 			val.sort()
-			if val not in dd[l]:
-				dd[l].append([cnf])
-				dd[l].append(val)
+			#если такой конфигурации решений еще нет по ключу "количество решений", то добавляем как вложенный словарь {кнф: конфигурация}
+			if val not in dd[l].values():
+				info = dd[l].setdefault(f'{cnf}',val)
+			#если конфигурация уже есть, то находим соответствующий ей кнф файл и удаляем его	
 			else:
 				print(cnf)
-				os.remove(f'{cnf[1:-1]}') 
+				os.remove(f'{cnf[1:-1]}')
+			#перемещаем каретку по массиву на кол-во строк конфигурации(кол-во решений) вниз, то есть к след строке формата "количество решений и имя кнф"
 			i = i + l			
 	f.close()
+#	print(*dd)
+
+	#сортируем словарь по ключам - количествам решений для удобства
 	d = dict(sorted(dd.items()))
+#	print(d)
+
+	#БЛОК ПОДСЧЁТА ИНВЕРСНЫХ ПАР
+	#скачем по ключам - кол-ву решений
 	for key in d.keys():
-		result.write(f'{key}: \n')
-		lstvals = d.get(key)
-		for k in range(len(lstvals)):
+		#пишем в файл res кол-во решений:
+		result.write(f'{key}:\n')
+
+		#перебираем список значений по каждому словарю {кнф:список конфигураций}
+		for cnfname in d.get(key):
+			#пишем в файл res имя кнф:
+			result.write(f'{cnfname}\n')
+			print(cnfname)
+
+			#записываем конфигурацию в список
+			configuration = list(d.get(key).get(cnfname))
+			print(configuration)
+			#создаем массив массивов, куда будем перезаписывать каждую строку конфигурации (она в формате строки) в массив интов 
 			res = []
-			for elem in lstvals[k]:
-				result.write(f'{elem}\n')
-				if elem[0] != '(':
-					s = elem.split(', ')
+			#
+			for k in range(len(configuration)):
+				
+				#
+				for elem in configuration[k]:
+					s = configuration[k].split(', ')
+					#print(s)
 					for m in range(len(s)):
 						s[m] = int(s[m])
 					res.append(s)		
-			cntinv = 0
-			invers = []
-			for i in range(len(res)):
-				temp = [-x for x in res[i]]
-				for j in range(len(res)):
-					if res[i] not in invers and res[j] == temp and i != j:
-						invers.append(res[i])
-						invers.append(temp)
-						cntinv += 1
-			if (list(lstvals[k])).index(elem) > 0:
-				result.write(f'Количество инверсных пар: {cntinv}:\n')
-				if cntinv in [1, 3, 5]:
-					URA = open('URA' + N + '.txt', "r")
-					URA.write(f'{key} \n {lstvals}')
-					URA.close()
-			for i in range(len(invers)):
-				for j in range(len(invers[i])):
-					result.write(f'{invers[i][j]} ')
-				result.write('\n')
+				cntinv = 0
+				invers = []
+				for i in range(len(res)):
+					temp = [-x for x in res[i]]
+					for j in range(len(res)):
+						if res[i] not in invers and res[j] == temp and i != j:
+							invers.append(res[i])
+							invers.append(temp)
+							cntinv += 1
+				if (list(configuration[k])).index(elem) > 0:
+					result.write(f'Количество инверсных пар: {cntinv}:\n')
+					if cntinv in [1, 3, 5]:
+						URA = open('URA' + N + '.txt', "a")
+						URA.write(f'{key} \n {configuration}\n')
+						URA.close()
+				for i in range(len(invers)):
+					for j in range(len(invers[i])):
+						result.write(f'{invers[i][j]} ')
+					result.write('\n')
 	result.close()
 	
 
